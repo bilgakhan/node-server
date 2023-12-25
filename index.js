60b6866ba19a938d6b68d1ab77b0ee53a63ef8ff
Anvar <anvar@bilgakhan.uz>
2023-12-25 22:52:40 +0500

    12-22 22:52 commit

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.post("/register", (req, res) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const { email, password, f_name, l_name } = req.body;
   const token = "qwiefgwiefgv$iuwbgiwurgfriubgwon_";
-  const user = "flutter@gmail.com";
-  console.log(email);
+
   if (!(email && password)) {
     res.status(400).send({
       status: 400,
@@ -23,7 +22,7 @@ app.post("/register", (req, res) => {
   }
 
   for (let i = 0; i < users.length; i++) {
-    if (email === users[i]) {
+    if (email === users[i]["email"]) {
       res.send({
         status: 200,
         message: "Bu email avval ro'yxatdan o'tgan",
@@ -40,6 +39,38 @@ app.post("/register", (req, res) => {
   res.end();
 });
 
+// login
+
+app.post("/login", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  const { email, password } = req.body;
+  if (!(email && password)) {
+    res.status(400).send({
+      status: 400,
+      message: "email va parol kiritilishligi shart",
+    });
+  }
+
+  for (let i = 0; i < users.length; i++) {
+    if (email === users[i]["email"]) {
+      if (password === users[i]["password"]) {
+        res.status(200).json({
+          message: "akkountga kirdigiz",
+          accessToken: "wieuyfgiweugfi$oborebwbwgrbo@",
+        });
+      } else {
+        res.status(400).json({
+          error: "Parol yoki login xato!",
+        });
+      }
+    }
+  }
+
+  res.end();
+});
+
 // get users
 app.get("/users", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -47,6 +78,15 @@ app.get("/users", (req, res) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.send(users);
 });
+
+// put users
+// app.put("/put/:id", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   const { email, password } = req.body;
+//   console.log(req.params.id);
+// });
 
 // IP settings
 const IP = "192.168.100.10";
